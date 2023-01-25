624387d4635b99007f06eda39e8f2e6bb231321c
sandipmatere1111 <sandipmatere1111@gmail.com>
2023-01-25 16:49:18 +0530

    List with Exception

--- 1Arraylist/src/Iterate.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class Iterate {
-    public static void main (String[] args){
-
-    }
-}
---
+++ 1Arraylist/src/SortArrayListException.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortArrayList {
+    public static void main(String[] args) {
+        try {
+            ArrayList<Integer> aL2 = new ArrayList<Integer>();
+            aL2.add(2);
+            aL2.add(4);
+            aL2.add(6);
+            aL2.add(8);
+            aL2.add(10);
+
+            Collections.sort(aL2);
+            System.out.println(aL2);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("EXception Thrown" +e);
+        }catch(IllegalArgumentException e){
+            System.out.println("Exception Thrown " +e);
+        }
+    }
+}
